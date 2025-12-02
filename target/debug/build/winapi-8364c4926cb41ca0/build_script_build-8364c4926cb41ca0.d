e2c9fa1dd5cd1f6b76695c0e7ff66ba9a2a11de3
zyenai <tyler.nw3bb@gmail.com>
2025-12-02 10:59:42 -0800

    Remove target/ and Cargo.lock from tracking

--- target/debug/build/winapi-8364c4926cb41ca0/build_script_build-8364c4926cb41ca0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\twebb\source\repos\addmin_dll\target\debug\build\winapi-8364c4926cb41ca0\build_script_build-8364c4926cb41ca0.exe: C:\Users\twebb\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-C:\Users\twebb\source\repos\addmin_dll\target\debug\build\winapi-8364c4926cb41ca0\build_script_build-8364c4926cb41ca0.d: C:\Users\twebb\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-C:\Users\twebb\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
--- target/debug/deps/addmin-2a522e277e3a213b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\twebb\source\repos\addmin_dll\target\debug\deps\libaddmin-2a522e277e3a213b.rmeta: src\lib.rs
-
-C:\Users\twebb\source\repos\addmin_dll\target\debug\deps\addmin-2a522e277e3a213b.d: src\lib.rs
-
-src\lib.rs:
